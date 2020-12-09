c08e1ae3b9f2310960e1d9e03039b781fc6ab3b1
leejaeoh <aaljo2@naver.com>
2020-12-10 05:53:15 +0900

    1210 morning

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import data from 'data';
 import Product from '../components/Product';
+import data from '../data';
 
 const HomeScreen = () => {
     return ( 
-    <div>
+        <div>
         <div className="row center">
           {
-            data.products.map(product =>(
-          <Product key= {product._id} product={product}></Product>
-            )) };
+            data.products.map(product=>(
+            <Product key={product._id} product={product}></Product>
+          ))};
         </div>
-    </div>
+      </div>
 );
 }
  
